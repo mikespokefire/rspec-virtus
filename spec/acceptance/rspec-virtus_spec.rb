eb387bccc913f08384c56a5b409ff15d647d466b
Michael Smith <mike@spokefire.co.uk>
2014-09-10 08:38:58 +0100

    Add acceptance specs

---
+++ spec/acceptance/rspec-virtus_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+require "virtus"
+
+class DummyPost
+  include Virtus.model
+
+  attribute :title, String
+  attribute :body, String
+  attribute :comments, Array[String]
+end
+
+describe DummyPost do
+  it { expect(described_class).to have_attribute(:title) }
+  it { expect(described_class).to have_attribute(:body).of_type(String) }
+  it { expect(described_class).to have_attribute(:comments).of_type(Array, member_type: String) }
+end
